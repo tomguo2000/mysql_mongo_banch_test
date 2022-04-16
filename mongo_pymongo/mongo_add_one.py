a389e494e16e85fb7a073872bc1477cfafaa00f1
guoliang7 <guoliang7@360.cn>
2022-04-16 16:37:06 +0800

    time

--- mongo_pymongo/mongo_add_one.py
+++ mongo_pymongo/mongo_add_one.py
@@ -1,4 +1,4 @@
-import datetime
+import time
 import json
 from .dbconfig import db
 from uuid import uuid4
@@ -7,7 +7,7 @@ def add_one_mongo():
     contents = {
         "message_id": uuid4().hex,
         "parameters": uuid4().hex,
-        "create_time": datetime.datetime.now()
+        "create_time": time.time()
     }
 
-    return db.event.insert_one(json.loads(contents))
+    return db.event.insert_one(json.dumps(contents))
