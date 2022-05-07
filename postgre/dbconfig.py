a0e9bb2a6ff8e8871b55438bc313c60a09ff88f6
guoliang7 <guoliang7@360.cn>
2022-05-08 00:37:26 +0800

    post to 127

--- postgre/dbconfig.py
+++ postgre/dbconfig.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-connection = psycopg2.connect(database='tomdb', user='tom', password='glnvod', host='47.100.138.122', port='5432')
+connection = psycopg2.connect(database='tomdb', user='tom', password='glnvod', host='127.0.0.1', port='5432')
 
 cur = connection.cursor()
 
